ef57590040b33fd59a2d320e7db6bd57aa207681
buwarohitt <buwarohitt@gmail.com>
2018-06-12 12:56:46 +0530

    Delete aftercicd.sh

--- aftercicd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd /var/lib/codedeploy/
-java -version
-javac HelloWorld.java
-java HelloWorld
-jar cvf HelloWorld.jar HelloWorld.class
-mkdir target
-mv *.jar target
-tar cvfz ${BUILD_NUMBER}-${JOB_NAME}.tar.gz target/*
